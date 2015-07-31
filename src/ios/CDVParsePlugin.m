1dec5ac5598d61842b476e28b8678a18311ddb9a
Shane Brinkman-Davis <shanebdavis@gmail.com>
2015-07-31 09:34:49 -0600

    have an initial, successful attempt to pass data back to javascript when didReceiveRemoteNotification happens

--- src/ios/CDVParsePlugin.m
+++ src/ios/CDVParsePlugin.m
@@ -25,6 +25,7 @@ options:
 
 - (void)register: (CDVInvokedUrlCommand*)command
 {
+    NSLog(@"CDVParsePlugin register 1");
     NSDictionary *options   = [command.arguments objectAtIndex:0];
     NSString *appId         = [options objectForKey:@"appId"];
     NSString *clientKey     = [options objectForKey:@"clientKey"];
@@ -33,6 +34,7 @@ options:
         [Parse setApplicationId:appId clientKey:clientKey];
 
     if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        NSLog(@"CDVParsePlugin register 2");
         UIUserNotificationSettings *settings =
             [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert |
                                                          UIUserNotificationTypeBadge |
@@ -50,6 +52,7 @@ options:
 
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    NSLog(@"CDVParsePlugin register done");
 }
 
 - (void)getInstallationId:(CDVInvokedUrlCommand*) command
@@ -136,8 +139,10 @@ void MethodSwizzle(Class c, SEL originalSelector) {
 
 + (void)load
 {
+    NSLog(@"CDVParsePlugin loading...");
     MethodSwizzle([self class], @selector(application:didRegisterForRemoteNotificationsWithDeviceToken:));
     MethodSwizzle([self class], @selector(application:didReceiveRemoteNotification:));
+    NSLog(@"CDVParsePlugin loaded");
 }
 
 - (void)noop_application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken
@@ -147,22 +152,52 @@ void MethodSwizzle(Class c, SEL originalSelector) {
 - (void)swizzled_application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken
 {
     // Call existing method
+    NSLog(@"CDVParsePlugin didRegisterForRemoteNotificationsWithDeviceToken 1");
     [self swizzled_application:application didRegisterForRemoteNotificationsWithDeviceToken:newDeviceToken];
     // Store the deviceToken in the current installation and save it to Parse.
     PFInstallation *currentInstallation = [PFInstallation currentInstallation];
     [currentInstallation setDeviceTokenFromData:newDeviceToken];
     [currentInstallation saveInBackground];
+    NSLog(@"CDVParsePlugin didRegisterForRemoteNotificationsWithDeviceToken done");
 }
 
 - (void)noop_application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
 }
 
+
+
+if (! jsonData) {
+    NSLog(@"Got an error: %@", error);
+} else {
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+
 - (void)swizzled_application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
     // Call existing method
+    NSLog(@"CDVParsePlugin didReceiveRemoteNotification 1");
     [self swizzled_application:application didReceiveRemoteNotification:userInfo];
     [PFPush handlePush:userInfo];
+
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictionaryOrArrayToOutput
+                                                       options:0 //NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+                                                         error:&error];
+    if (jsonData) {
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        NSLog(@"CDVParsePlugin didReceiveRemoteNotification: userInfo: %@", jsonString);
+        NSString *callback = @"console.log"
+        NSString *jsCallBack = [NSString stringWithFormat:@"%@(%@);", callback, jsonString];
+        NSLog(@"CDVParsePlugin didReceiveRemoteNotification: jsCallBack: %@", jsCallBack);
+
+        [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
+    } else {
+        NSLog(@"Error converting Push data to json: %@", error)
+    }
+
+
+    NSLog(@"CDVParsePlugin didReceiveRemoteNotification done");
 }
 
 @end
